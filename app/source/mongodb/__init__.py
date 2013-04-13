ecd495c5d60db91e905de8d8f8efee035876b6ae
abhinav <abhinav@viralmint.com>
2013-04-13 23:42:15 +0530

    remove incomplete mongodb

--- app/source/mongodb/__init__.py
+++
@@ -1,6 +0,0 @@
-info = {
-    'type' : 'source',
-    'title' : 'MongoDB',
-    'icon' : 'http://intouching.com/images/iconMongo.png',
-    'config_url' : '#'
-}
--- app/source/mongodb/controller.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, session
-from peewee import DoesNotExist
-from app.core.model import User
-register = Blueprint('s_mongodb', __name__,
-                     template_folder='templates')
