163b12cca00161be310396fd3aaf1a32f9c05770
Abhinav <abhinav@socialdealfactory.com>
2013-04-18 19:59:55 +0530

    defult IP binding changed to global

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 
 from app import webackup
-webackup.run(host='127.0.0.1', debug=True, port=2587)  # Start the server
+webackup.run(host='0.0.0.0', debug=True, port=2587)  # Start the server
